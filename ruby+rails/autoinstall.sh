d398c1a9dd7be4f6429e2129567ac03314f61180
JakubBialoskorski <bialoskorski@icloud.com>
2018-04-30 22:36:04 +0200

    initial commit

---
+++ ruby+rails/autoinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+sudo apt-get update
+sudo apt-get install -y git curl zlib1g-dev subversion
+sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
+sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3
+sudo apt-get install -y libxml2-dev libxslt-dev
+sudo apt-get install -y autoconf automake libtool bison
+curl -L get.rvm.io | bash -s stable --rails
+source ~/.rvm/scripts/rvm
+gem install execjs
+sudo apt-get install nodejs
+ruby -v
+rails -v
