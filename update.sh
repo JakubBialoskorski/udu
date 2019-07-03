965246f41c7b5ca8270a15056ead70d8286e7099
JakubBialoskorski <saleen1451@gmail.com>
2019-07-03 12:08:57 +0000

    added reboot check after update and upgrade

--- update.sh
+++ update.sh
@@ -2,3 +2,5 @@
 date
 git pull
 sudo apt-get -q update && sudo apt-get -qy upgrade && sudo apt-get -qy dist-upgrade && sudo apt -qy autoremove && sudo apt-get -qy autoclean
+echo "[PACKAGES REQUESTING FOR A REBOOT:]"
+cat /var/run/reboot-required.pkgs
