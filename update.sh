afb1eb35cab26cd7e4b7cac779e192f0fc167f11
Jakub Białoskórski <bialoskorski@icloud.com>
2022-01-13 15:39:29 +0100

    allow releaseinfo change automatically

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date
 git pull
-sudo apt-get -q update && sudo apt-get -qy upgrade && sudo apt-get -qy dist-upgrade && sudo apt -qy autoremove && sudo apt-get -qy autoclean
+sudo apt-get -q update --allow-releaseinfo-change && sudo apt-get -qy upgrade && sudo apt-get -qy dist-upgrade && sudo apt -qy autoremove && sudo apt-get -qy autoclean
 echo "[PACKAGES REQUESTING FOR A REBOOT:]"
 cat /var/run/reboot-required.pkgs
